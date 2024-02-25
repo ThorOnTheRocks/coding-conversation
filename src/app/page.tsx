67f7ad85d15eb85ef9ceefdc487235fdd8f222fe
gianlucagalota <gianluca.galota@gmail.com>
2024-02-25 17:07:53 +0100

    removed unusued import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { Button } from '@/components/ui/button';
-import { signIn } from '@/actions/signIn';
-import { signOut } from '@/actions/signOut';
-import { auth } from '@/auth';
-
 export default async function Home() {
-  const session = await auth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Homepage
